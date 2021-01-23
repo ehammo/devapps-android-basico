c383f31fe26b98d58074c0889b4c7a75c1d9bd51
Eduardo <ehammo@motorola.com>
2021-01-23 11:43:00 -0300

    add types of alert dialog

--- app/src/main/java/cesar/school/devapps20211_helloworld/MainActivity.kt
+++ app/src/main/java/cesar/school/devapps20211_helloworld/MainActivity.kt
@@ -1,10 +1,14 @@
 package cesar.school.devapps20211_helloworld
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import cesar.school.devapps20211_helloworld.adapter.EstadoAdapter
@@ -67,7 +71,49 @@ class MainActivity : AppCompatActivity() {
 
 	override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
 		R.id.action_capitais -> {
-			startActivity(Intent(this, CapitaisActivity::class.java))
+			val onTimeListener = TimePickerDialog.OnTimeSetListener {
+					_: TimePicker, hour: Int, min: Int ->
+					Toast.makeText(this@MainActivity,
+						"Hora escolhida $hour, Minuto $min", Toast.LENGTH_SHORT).show()
+			}
+			val timeDialog = TimePickerDialog(this, onTimeListener, 0, 0, false)
+//			timeDialog.show()
+
+			val onDateListener = DatePickerDialog.OnDateSetListener {
+					view, year, month, dayOfMonth ->
+				val actualMonth = if (month < 9) {
+					"0${month + 1}"
+				} else {
+					"${month + 1}"
+				}
+				Toast.makeText(this@MainActivity,
+					"Data escolhida $dayOfMonth/$actualMonth/$year", Toast.LENGTH_SHORT).show()
+			}
+			// Mes vai de 0-11
+			val datePickerDialog = DatePickerDialog(this, onDateListener, 2021, 0, 23)
+			datePickerDialog.show()
+
+			val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+			builder.apply {
+				setTitle("Aviso!")
+				setMessage("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+				setCancelable(false)
+				setPositiveButton("Ok") { _, _ ->
+					Toast.makeText(this@MainActivity, "Dialog ok", Toast.LENGTH_SHORT).show()
+				}
+				setNegativeButton("close") { dialog, _ ->
+					dialog.cancel()
+				}
+				setNeutralButton("E se o nome do botao for grande?") {dialog, _ ->
+					dialog.dismiss()
+				}
+			}
+
+
+			val alertDialog = builder.create()
+//			alertDialog.show()
+
+//			startActivity(Intent(this, CapitaisActivity::class.java))
 			true
 		}
 
